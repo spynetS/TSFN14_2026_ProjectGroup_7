cd33d3b3c911e02aad8ff677aeb2aa77e9d99567
Alfred <alfred@stensatter.se>
2026-02-24 21:31:05 +0100

    terraform working in minikube

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,7 @@
-resource "kubernetes_pod" "nginx" {
-  metadata {
-    name = "nginx"
-    labels = {
-      app = "nginx"
-    }
-  }
-
-  spec {
-    container {
-      image = "nginx:1.23"
-      name  = "nginx"
-      port {
-        container_port = 80
-      }
-    }
-  }
+resource "kubernetes_manifest" "deployment" {
+  manifest = yamldecode(file("../backend/deployment.yaml"))
 }
 
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-service"
-  }
-
-  spec {
-    selector = {
-      app = kubernetes_pod.nginx.metadata[0].labels.app
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "NodePort"
-  }
+resource "kubernetes_manifest" "service" {
+  manifest = yamldecode(file("../backend/service.yaml"))
 }
