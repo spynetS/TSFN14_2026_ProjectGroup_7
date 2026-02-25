5425e4de59470a7fbcade520451cb15943a1e422
Alfred <alfred@stensatter.se>
2026-02-25 21:00:58 +0100

    moved manifests

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,16 @@
 resource "kubernetes_manifest" "deployment" {
-  manifest = yamldecode(file("../backend/manifests/deployment.yaml"))
+  manifest = yamldecode(file("../manifests/deployment.yaml"))
 }
 
 resource "kubernetes_manifest" "mongo-deployment" {
-  manifest = yamldecode(file("../backend/manifests/mongo-deployment.yaml"))
+  manifest = yamldecode(file("../manifests/mongo-deployment.yaml"))
 }
 
 resource "kubernetes_manifest" "service" {
-  manifest = yamldecode(file("../backend/manifests/service.yaml"))
+  manifest = yamldecode(file("../manifests/service.yaml"))
 }
 
 resource "kubernetes_manifest" "mongo-service" {
-  manifest = yamldecode(file("../backend/manifests/mongo-service.yaml"))
+  manifest = yamldecode(file("../manifests/mongo-service.yaml"))
 }
 
